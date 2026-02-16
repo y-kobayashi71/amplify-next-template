3b52ab8692e23eac6383c2e09355993de7494f1d
小林優斗 <y-kobayashi@fusic.co.jp>
2026-02-16 14:21:57 +0900

    import時のjs拡張子を削除

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-import { helloAmplify } from './functions/helloamplify/resources.js';
+import { helloAmplify } from './functions/helloamplify/resources';
 
 defineBackend({
   auth,
